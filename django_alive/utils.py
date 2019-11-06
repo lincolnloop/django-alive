fa06e5639b2fdc97cd5362de6142f52915e240c8
Peter Baumgartner <pete@lincolnloop.com>
2019-11-05 21:28:45 -0700

    More typing

--- django_alive/utils.py
+++ django_alive/utils.py
@@ -3,11 +3,19 @@ from django.utils.module_loading import import_string
 
 from . import HealthcheckFailure
 
+# Ignore typing on Python <3.5
+try:
+    from typing import List
+except ImportError:
+    pass
+
+
 DEFAULT_ALIVE_CHECKS = {"django_alive.checks.check_database": {}}
 ALIVE_CHECKS = getattr(settings, "ALIVE_CHECKS", DEFAULT_ALIVE_CHECKS)
 
 
 def perform_healthchecks():
+    # typing: () -> (bool, List[str])
     errors = []
     for func, kwargs in ALIVE_CHECKS.items():
         try:
