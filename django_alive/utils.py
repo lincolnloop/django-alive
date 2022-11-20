6d66a60220f2086ee94b9fe53c8eac643be02b11
Petr Dlouhý <petr.dlouhy@email.cz>
2022-11-20 11:48:50 +0100

    allow to execute one test multiple times with different parameters

--- django_alive/utils.py
+++ django_alive/utils.py
@@ -1,3 +1,4 @@
+import warnings
 from django.conf import settings
 from django.utils.module_loading import import_string
 
@@ -10,14 +11,24 @@ except ImportError:
     pass
 
 
-DEFAULT_ALIVE_CHECKS = {"django_alive.checks.check_database": {}}
-ALIVE_CHECKS = getattr(settings, "ALIVE_CHECKS", DEFAULT_ALIVE_CHECKS)
+DEFAULT_ALIVE_CHECKS = [("django_alive.checks.check_database", {})]
 
 
 def perform_healthchecks():
     # typing: () -> (bool, List[str])
     errors = []
-    for func, kwargs in ALIVE_CHECKS.items():
+    ALIVE_CHECKS = getattr(settings, "ALIVE_CHECKS", DEFAULT_ALIVE_CHECKS)
+    if isinstance(ALIVE_CHECKS, dict):
+        # Deprecated dict format
+        warnings.warn(
+            "ALIVE_CHECKS should be a list of tuples, not a dict. "
+            "Please update your settings.",
+            DeprecationWarning,
+        )
+        checks = ALIVE_CHECKS.items()
+    else:
+        checks = ALIVE_CHECKS
+    for func, kwargs in checks:
         try:
             import_string(func)(**kwargs)
         except HealthcheckFailure as e:
